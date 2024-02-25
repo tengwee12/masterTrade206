07f09addd17249754fb43515381c9988ac871170
tengwee12 <tengwee.ong.2022@scis.smu.edu.sg>
2024-02-25 15:51:29 +0800

    added sql support

--- backend/apps/issuePosting/issue.js
+++ backend/apps/issuePosting/issue.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
     res.send('post backend is running!');
@@ -9,3 +9,28 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+const mysql = require('mysql');
+
+// Create connection
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'issuePostingDB'
+});
+
+// Connect
+db.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log('MySQL connected...');
+});
+
+// Create a route to test the connection
+app.get('/', (req, res) => {
+  res.send('MySQL connection established!');
+});
+
+
