41445203aa8a8ea71ac1b92a63cfd54351503239
Ryan <ryanlimjk@gmail.com>
2024-02-29 01:45:38 +0800

    added JWTtoken verification function, WIP

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -21,3 +21,22 @@ function issueJWT(user) {
 }
 
 module.exports.issueJWT = issueJWT;
+
+//verification for JWT
+function verifyJWT(req, res, next) {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({ message: 'Please log in first!' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, "secret"); 
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(403).json({ message: 'Invalid token' });
+  }
+}
+
+module.exports.verifyJWT = verifyJWT;
