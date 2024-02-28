a1e1d9b59e357323a64b56a8d85dfa46ee0c69ad
Hui Feng <tristan.chf@gmail.com>
2024-02-28 20:00:46 +0800

    login system

---
+++ backend/apps/user/user.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+const passport = require("passport");
+const auth = require("../../middleware/auth");
+
+const User = require("./model");
+
+router.get('/protected', passport.authenticate('jwt', { session: false }), (req, res, next) => {
+    res.status(200).json({ success: true, msg: "You are successfully authenticated to this route!"});
+});
+
+router.get("/login", async (req, res, next) => {
+    User.findOne({ email: req.body.email })
+    .then((user) => {
+        if (!user) {
+            return res.status(401).json({ success: false, msg: "could not find user" });
+        }
+
+        if (user.password === req.body.password) {
+            const tokenObject = auth.issueJWT(user);
+            res.status(200).json({ success: true, token: tokenObject.token, expiresIn: tokenObject.expires });
+
+        } else {
+            res.status(401).json({ success: false, msg: "you entered the wrong password" });
+
+        }
+
+    })
+    .catch((err) => console.log(err));
+
+});
+
+module.exports = router;
