ed0179d16e25714f96421b8502595bc8a56eac5f
innumii <linus.phong.2022@scis.smu.edu.sg>
2024-02-28 23:13:54 +0800

    fixed the padding issue w message -inu

--- frontend/pages/chat/Message.js
+++ frontend/pages/chat/Message.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 
 const Message = ({ message, time, isSent }) => {
   return (
-    <View className={`bg-white p-2 rounded-lg ${isSent ? 'self-end bg-purple-300' : 'self-start bg-purple-100'}`}>
+    <View style={styles.item} className={`bg-white p-2 rounded-lg ${isSent ? 'self-end bg-purple-300' : 'self-start bg-purple-100'}`}>
       <Text className={`text-black`}>{message}</Text>
       <Text className={`text-xs text-gray-500 mt-1 ${isSent ? 'self-start' : 'self-end'}`}>{time}</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  item: {
+    marginVertical: 8,
+  },
+});
 
 export default Message;
