b4ac1c17d84e226e48a5437f1e97d1e1e63ed693
Ryan <ryanlimjk@gmail.com>
2024-03-03 22:30:53 +0800

    added attributes to plumber + tidy up

--- backend/apps/plumber/model.js
+++ backend/apps/plumber/model.js
@@ -2,13 +2,33 @@ const { DataTypes } = require('sequelize');
 const db = require('../../config/db');
 
 const Plumber = db.define('Plumber', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
     email: {
         type: DataTypes.STRING,
-        primaryKey: true
+        validate: {
+            isEmail: true
+        },
     },
     password:{
         type:DataTypes.STRING,
+    },
+    description:{
+        type:DataTypes.TEXT
+    },
+    license:{
+        type:DataTypes.BOOLEAN
+    },
+    image:{
+        type:DataTypes.STRING,
+        validate: {
+            isUrl: true
+        }
     }
+
 });
 
 module.exports = Plumber;
--- backend/apps/user/model.js
+++ backend/apps/user/model.js
@@ -2,12 +2,16 @@ const { DataTypes } = require('sequelize');
 const db = require('../../config/db');
 
 const User = db.define('User', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
     email: {
         type: DataTypes.STRING,
         validate: {
             isEmail: true
         },
-        primaryKey: true
     },
     password:{
         type:DataTypes.STRING,
