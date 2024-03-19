018d5d26116e49898d6da74e259c4b242508d46e
Sharlene Tio <sharlenetio.2021@scis.smu.edu.sg>
2024-03-19 15:21:42 +0800

    remove scrollview for job listings

--- frontend/pages/post/PlumberPostsPage.js
+++ frontend/pages/post/PlumberPostsPage.js
@@ -1,7 +1,5 @@
 import {
-  ScrollView,
   FlatList,
-  Text,
   View,
 } from "react-native";
 import { useState, useEffect } from "react";
@@ -30,7 +28,7 @@ export default function PlumberPostsPage() {
   }, []);
 
   return (
-    <ScrollView>
+    <View>
       <View className="absolute left-0 right-0 top-0 h-24 bg-brandPurple"></View>
       <Logo text="Job Listings"/>
       <FlatList
@@ -40,6 +38,6 @@ export default function PlumberPostsPage() {
         )}
         keyExtractor={(item) => item.id.toString()}
       />
-    </ScrollView>
+    </View>
   );
 }
